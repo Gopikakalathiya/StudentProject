ce334ee633f19215fa0db5e3221f348eb431efaf
GopikaKalathiya <kalathgo@sheridancollege.ca>
2022-05-26 09:35:21 -0400

    Version 1.0 of student project by using Array of Object

--- src/softwarefundamental/TestFriends.java
+++ src/softwarefundamental/TestFriends.java
@@ -10,10 +10,11 @@ public class TestFriends {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Friends[] f=new Friends[3];
+        Friends[] f=new Friends[4];
         f[0] = new Friends("John",20);
         f[1] = new Friends("peter",19);
         f[2] = new Friends("Siva",19);
+        f[3] = new Friends("Gopi",25);
 
         for(Friends fr: f)
         {
